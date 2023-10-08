db052d6f701cac42791a1c20ac2c4a5a410b630a
Aakash Rajur <aakashrajur16@gmail.com>
2023-10-08 13:10:11 +0530

    rename entrypoint paths

--- cmd/sqlxgen/main.go
+++ cmd/sqlxgen/main.go
@@ -1,58 +1,20 @@
 package main
 
 import (
-	_ "embed"
-	"os"
-
-	"github.com/aakash-rajur/sqlxgen/internal/config"
+	"github.com/aakash-rajur/sqlxgen/internal/cli"
 	"github.com/aakash-rajur/sqlxgen/internal/utils"
-	"github.com/aakash-rajur/sqlxgen/internal/utils/fs"
-	"github.com/aakash-rajur/sqlxgen/internal/utils/writer"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/jmoiron/sqlx"
-	_ "github.com/lib/pq"
 )
 
 func main() {
-	workingDir, err := os.Getwd()
+	cmd := cli.RootCmd(Version)
 
-	if err != nil {
-		utils.ExitWithError(err)
+	err := cmd.Execute()
 
+	if err == nil {
 		return
 	}
 
-	fd := fs.NewFileDiscovery()
-
-	wc := writer.NewFileWriter
-
-	connect := sqlx.Connect
-
-	sqlxGenCfg, err := config.NewSqlxGen(
-		config.SqlxGenArgs{
-			Connect:       connect,
-			Fd:            fd,
-			WriterCreator: wc,
-			WorkingDir:    workingDir,
-			SqlxAltPath:   "",
-		},
-	)
-
-	if err != nil {
-		utils.ExitWithError(err)
-
-		return
-	}
-
-	sqlxGenCfg.InitLogger()
-
-	err = sqlxGenCfg.Generate()
-
-	if err != nil {
-		utils.ExitWithError(err)
-
-		return
-	}
-
-	println("all done!")
+	utils.ExitWithError(err)
 }
+
+var Version = "dev"
